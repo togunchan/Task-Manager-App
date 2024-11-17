dfef07e7af199498562df6c8566c91ed6e3e7a63
togunchan <togunchan@gmail.com>
2024-11-17 22:24:25 +0300

    Added a controller and a router

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+app.use(express.json())
 
 // routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')       -get all the data
+// app.post('/api/v1/tasks')      -create a new task
+// app.get('/api/v1/tasks')       -get single data
+// app.patch('/api/v1/tasks')     -update task
+// app.delete('/api/v1/tasks')    -delete task
 
 
 const port = 5001
---
+++ controllers/tasks.js
@@ -0,0 +1,29 @@
+const { create, update } = require("lodash")
+
+const getAllTasks = (req, res) => {
+    res.send('Get All Tasks')
+}
+
+const createTask = (req, res) => {
+    res.send('Create Task')
+}
+
+const getTask = (req, res) => {
+    res.send('Get Single Task')
+}
+
+const updateTask = (req, res) => {
+    res.send('Update Task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Delete Task')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
